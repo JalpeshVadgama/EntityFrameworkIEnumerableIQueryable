e4194bd84c857d44c14729261cae08fa8764f3e3
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-03-21 15:53:18 +0530

    Added Data Context class and connectionstring

--- EntityFrameworkIQAndIM/EntityFrameworkIQAndIM/Stduent.cs
+++ EntityFrameworkIQAndIM/EntityFrameworkIQAndIM/Stduent.cs
@@ -1,7 +1,10 @@
-﻿namespace EntityFrameworkIQAndIM
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkIQAndIM
 {
     public class Stduent
     {
+        [Key]
         public int  StudentId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
---
+++ EntityFrameworkIQAndIM/EntityFrameworkIQAndIM/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace EntityFrameworkIQAndIM
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext()
+            : base("StudentConnectionString")
+        {
+            
+        }
+        public DbSet<Stduent> Students { get; set; }
+    }
+}
