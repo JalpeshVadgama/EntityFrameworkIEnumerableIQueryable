7c57dbcbe3a66d1ed3be9ee0a56cf47236536ecc
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-03-21 15:48:34 +0530

    Initial Commit

---
+++ EntityFrameworkIQAndIM/EntityFrameworkIQAndIM/Stduent.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFrameworkIQAndIM
+{
+    public class Stduent
+    {
+        public int  StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Percentage { get; set; }
+    }
+}
